196d7f82b444932d1d4339eaae47a8f32326f20f
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-10-03 02:39:48 -0700

    Refactor player movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     public float velocity = 3.0f;
-    private Vector3 direction;
 
     void Update() {
-        direction.x = Input.GetAxis("Horizontal");
-        direction.y = 0.0f;
-        direction.z = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
 
         transform.localPosition += direction * velocity * Time.deltaTime;
     }
