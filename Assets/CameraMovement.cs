ffd4b7a8d7472bbe9194e24e5718e6d3439a152a
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-10-03 00:42:05 -0700

    Follow player with camera

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -9,11 +9,37 @@ public class CameraMovement : MonoBehaviour {
     [SerializeField, Range(1.0f, 20.0f)]
     float distance = 5.0f;
 
+    [SerializeField, Min(0f)]
+    float focusRadius = 1.0f;
+
+    [SerializeField, Range(0f, 1f)]
+    float focusCentering = 0.5f;
+
+    Vector3 focusPoint;
+
     void Awake() {
-        
+        focusPoint = focus.position;
     }
 
-    void Update() {
-        
+    void LateUpdate() {
+        CalculateFocusPoint();
+        Vector3 lookDirection = transform.forward;
+        transform.localPosition = focusPoint - lookDirection * distance;        
+    }
+
+    void CalculateFocusPoint() {
+        float t = 1f;
+        Vector3 targetPoint = focus.position;
+        if (focusRadius > 0f) {
+            float distance = Vector3.Distance(targetPoint, focusPoint);
+            if (distance > 0.01f && focusCentering > 0f) {
+                t = Mathf.Pow(1f - focusCentering, Time.unscaledDeltaTime);
+            }
+            if (distance > focusRadius) {
+                t = Mathf.Min(t, focusRadius / distance);
+            }
+        }
+
+        focusPoint = Vector3.Lerp(targetPoint, focusPoint, t);
     }
 }
